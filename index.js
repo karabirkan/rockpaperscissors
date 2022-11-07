01a51739ca689ed0c7e47e14dc76369d242ab56f
tokyoparis <115654236+tokyoparis@users.noreply.github.com>
2022-11-07 13:02:28 -0500

    Computer random pick and solution function added

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
+// Computer will pick one of those value in the answer variable. 
+let answer = ["rock", "paper", "scissors"];
 
-let answer = ["rock","paper","scissors"];
+let getComputerChoice = answer[Math.floor(Math.random() * answer.length)];
 
-let getComputerChoice = answer[Math.floor(Math.random()* answer.length)];
+// We will find the winner and the looser with this playRound function.
+function playRound(playerSelection, computerSelection) {
+
+    //let playerSelection = prompt("What do you pick?");
+    // Rock wins against Scissors.
+    // Scissors wins against Paper.
+    // Paper wins against Rock.
+
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+
+        return "Player Won";
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "rock" ||
+        playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "scissors") {
+
+        return "Player Lost"
+
+    }
+    else {
+
+        return "Try Again. You both picked the same answer!";
+    }
+
+}
 
 
-function playRound(playerSelection, computerSelection){
+/*console.log(playRound(getComputerChoice,getComputerChoice));
 
-        if (playerSelection === "rock" && computerSelection === "scissors"){
 
-            return "what??";
-        }
-        else {
-    
-            return "You Lose! Paper beats Rock";
-        }
-        
-        }
-        
-    
-         
-playRound("rock",getComputerChoice);
+playRound("",getComputerChoice);
 
 
 
@@ -36,13 +53,13 @@ console.log(playRound)
 
 
 function game(){
-for(let i=0; i<5; i++){
+for(let i=0; i<155; i++){
 
 
-  //  alert(playRound("rock", getComputerChoice));
-
-}
+ console.log(playRound("rock", getComputerChoice));
 
 }
 
-game(); 
+}
+
+game();  */
