3f34de099e68d67bfa67c8c4f0b31bc1ef765085
tokyoparis <115654236+tokyoparis@users.noreply.github.com>
2022-11-07 10:21:51 -0500

    single round game

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function playRound(playerSelection, computerSelection){
+
+    playerSelection;
+    computerSelection;
+    
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+
+        alert("what??");
+    }
+    else {
+
+        alert("you lost");
+    }
+    
+    }
+    
+    
+         
+
+playRound("rock","scissors");
